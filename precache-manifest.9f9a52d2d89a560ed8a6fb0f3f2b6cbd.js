fc77c17fa149d9aa5dd2095cdaff4ef03bce8401
William B <william.benn11@gmail.com>
2019-08-07 15:24:03 -0400

    Added Build Files

---
+++ precache-manifest.9f9a52d2d89a560ed8a6fb0f3f2b6cbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "561348a97aeaa0d8cdfb8c318bc42fee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "435349ab5da35fa8ea71",
+    "url": "/static/css/main.dd0c19d4.chunk.css"
+  },
+  {
+    "revision": "dacd09cbfcb0a494cce7",
+    "url": "/static/js/2.fe2e18a1.chunk.js"
+  },
+  {
+    "revision": "435349ab5da35fa8ea71",
+    "url": "/static/js/main.0c34a120.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0d0345f662a757ef17eef497fa8e83fc",
+    "url": "/static/media/spinning.0d0345f6.gif"
+  }
+]);
